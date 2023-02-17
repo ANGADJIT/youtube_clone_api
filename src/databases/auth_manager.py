198675b91b7e0ff035bc599d962159103433fb4b
Angadjit singh <angad38.agcs@gmail.com>
2023-02-17 09:51:49 +0530

    started auth_managers and assigned task .

--- src/databases/auth_manager.py
+++ src/databases/auth_manager.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from utils.aws_manager import AWSManager
 from models.models import UserInfo
+from datetime import datetime
 from utils.jwt_token_manager import JwtTokenManger
 
 
@@ -19,10 +20,25 @@ class AuthManager:
 
         if check is None:
             # make None profile photo in copy data
-            data = auth_data.copy()
-            data.update('profile_photo', None)
 
-            auth_user: UserInfo = UserInfo(**data)
+            auth_user: UserInfo = UserInfo(**auth_data)
 
-            self.__db.add(auth_data)
+            self.__db.add(auth_user)
             self.__db.commit()
+
+            # get user id
+            user = self.__db.query(UserInfo).filter(
+                UserInfo.email == auth_data['email']).first()
+
+            # generate access token
+            access_token: str = self.__token_manager.generate_jwt_token(
+                {'user_id': user.id})
+
+            return {
+                'access_token': access_token,
+                'created_at': str(datetime.now())
+            }
+        
+        # TODO :: ANMOL IMPLEMENT LOGIN LOGIC HERE
+        else:
+            pass
