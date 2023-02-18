a324f8a785a220fc00ce0c98443a570dcd6d99ae
Angadjit singh <angad38.agcs@gmail.com>
2023-02-18 23:32:56 +0530

    added gister route in auth and added task for login route

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from utils.base_postgres_orm import base_postgres_orm
+from routes.auth import AuthRouter
 
 # *** YOUTUBE CLONE API ***
 
 api: FastAPI = FastAPI()
+
+# all routers objects
+auth_router: AuthRouter = AuthRouter()
+
+api.include_router(auth_router.router)
 
 
 @api.get('/')
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,4 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException, status
+from databases.auth_manager import AuthManager
+from schemas.schemas import AuthCreate, AuthResponse
+from sqlalchemy.orm import Session
+from utils.base_postgres_orm import base_postgres_orm
 
 
-router: APIRouter = APIRouter(tags=['Auth'])
+class AuthRouter:
+
+    def __init__(self) -> None:
+        self.__router: APIRouter = APIRouter(prefix='/auth', tags=['Auth'])
+
+        # register routes
+        self.__register_routes()
+
+    # getter for router object
+    @property
+    def router(self) -> APIRouter:
+        return self.__router
+
+
+    def __register_routes(self) -> None:
+
+        @self.__router.post('/register', status_code=status.HTTP_201_CREATED, response_model=AuthResponse)
+        def register(auth: AuthCreate = Depends(), file: UploadFile = File(...), db: Session = Depends(base_postgres_orm.db)):
+            auth_manager = AuthManager(db)
+
+            result: dict | None = auth_manager.register(
+                auth_data=auth.dict(), file=file.file)
+
+            if result is None:
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail={
+                                    'error': f'user with {auth.email} already exists'})
+            
+            return result
+
+    # TODO @Anmol create login route here -> /login
